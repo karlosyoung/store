4d5cc6d310afa4a6cc0e6ec73b1167d0ee3ef601
caoxiaolong <caoxiaolong@163.com>
2017-02-07 16:20:23 +0800

    提交代码

--- SunSoft/app/src/main/java/com/sunsoft/zyebiz/b2e/mvp/login/LoginFragment.java
+++ SunSoft/app/src/main/java/com/sunsoft/zyebiz/b2e/mvp/login/LoginFragment.java
@@ -2,13 +2,14 @@ package com.sunsoft.zyebiz.b2e.mvp.login;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.sunsoft.zyebiz.b2e.R;
 import com.sunsoft.zyebiz.b2e.common.ui.CommonPag;
 import com.sunsoft.zyebiz.b2e.mvp.base.BaseFragment;
 import com.sunsoft.zyebiz.b2e.utils.localUtil.UIUtil;
-
-import static u.aly.x.R;
 
 /**
  * Created by Amoly.
@@ -18,7 +19,15 @@ import static u.aly.x.R;
 public class LoginFragment extends BaseFragment {
 
     private View mLoginView;
-    private View mViewById;
+    private Button mButton;
+    private FrameLayout mUsename_layout;
+    private Button mBt_usename_clear;
+    private EditText mUsename;
+    private FrameLayout mPwd_layout;
+    private EditText mEdt_passwd;
+    private Button mTv_login;
+    private TextView mTv_register;
+    private TextView mForget_pas;
 
     @Override
     protected void clearData() {
@@ -75,14 +84,20 @@ public class LoginFragment extends BaseFragment {
 
             }
         };
+        commonPag.showSuccessedView();
         return commonPag;
     }
 
     private void initSubView() {
-       View mUsename = (View) mLoginView.findViewById(R.id.username_layout);
-       Button mBt_usename_clear = (Button) mLoginView.findViewById(R.id.bt_username_clear);
-
-
+        mUsename_layout = (FrameLayout) mLoginView.findViewById(R.id.username_layout);
+        mBt_usename_clear = (Button) mLoginView.findViewById(R.id.bt_username_clear);
+        mUsename = (EditText) mLoginView.findViewById(R.id.username);
+        mPwd_layout = (FrameLayout) mLoginView.findViewById(R.id.pwd_layout);
+        mButton = (Button) mLoginView.findViewById(R.id.img_pass_show);
+        mEdt_passwd = (EditText) mLoginView.findViewById(R.id.edt_passwd);
+        mTv_login = (Button) mLoginView.findViewById(R.id.tv_login);
+        mTv_register = (TextView) mLoginView.findViewById(R.id.tv_register);
+        mForget_pas = (TextView) mLoginView.findViewById(R.id.forget_pas);
     }
 
 
